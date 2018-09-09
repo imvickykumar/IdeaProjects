302faa285dcdf1acb7bcc01677f020ce60fec3fd
Vicky Kumar <vickythetechguy@gmail.com>
2018-09-10 01:33:27 +0530

    Stack with updates

--- Stack/src/com/company/Main.java
+++ Stack/src/com/company/Main.java
@@ -5,13 +5,12 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 
-        Stack theStack = new Stack(3);   //size is passed here only;
+        MyStack theStack = new MyStack(3);   //size is passed here only;
         theStack.push(10); theStack.display();
         theStack.push(20); theStack.display();
         theStack.push(30); theStack.display();
-        theStack.push(40); theStack.display();
-        theStack.push(50);theStack.display();
-
+        theStack.pop(); theStack.display();
+        theStack.push(20); theStack.display();
         theStack.display();
     }
 }
